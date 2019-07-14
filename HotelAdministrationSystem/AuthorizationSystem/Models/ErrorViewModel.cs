72c979d0d9fce345dbb6c70b63839f2be6ce7128
WizKhaalif <nikitaM37@yandex.ru>
2019-07-15 01:39:09 +0300

    добавил авторизацию

---
+++ HotelAdministrationSystem/AuthorizationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthorizationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HotelAdministrationSystem/AuthorizationSystem/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationSystem.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AuthorizationSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(x => x.SerializerSettings.Converters.Add(new StringEnumConverter()));
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseNpgsql(Configuration["DbConnectionString"],
+                    c => c.MigrationsAssembly("AuthorizationSystem"));
+            });
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("authapi", new Info { Title = "AuthorizationSystemApi", Version = "authapi" });
+                opt.DescribeAllEnumsAsStrings();
+            });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
